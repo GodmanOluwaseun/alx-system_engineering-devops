08ddd669a0bf0339c9dc68ad0cef465d83db5d89
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-07-12 03:27:07 -0700

    Client configuration with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 #Set the config of an ssh file
 
 file_line { ‘Set private key’:
-  Path => ‘~/.ssh/ssh_config’,
+  Path => ‘etc/ssh/ssh_config’,
   line => ’	IdentityFile ~/.ssh/school’,
   match => ‘^IdentityFile’, 
 }
 
 file_line { ‘Set password check off’.:
-  path => ‘~/.ssh/ssh_config’,
+  path => ‘etc/ssh/ssh_config’,
   line => ‘	PasswordAuthentication no’,
   match => ‘^PasswordAuthentication’,
 }
