db05d22ccbb0ea1a94cff7570af5db218e6213d8
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-07-17 13:35:11 -0700

    Puppet configure file for server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,27 @@
 #Puppet file to Configure servers and custom http header
 
-exec { 'configure-http':
-  command => 'apt-get update;
-  sudo apt-get -y install nginx;
-  sudo sed -i "/location \/ {/a \ add_header X-Served-By $hostname;" /etc/nginx/sites-available/default;
-  sudo service nginx restart',
-  provider => shell,
+exec { 'system_update':
+  command => '/usr/bin/apt-get update',
+  path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+}
+
+package { 'nginx':
+  ensure   => installed,
+  require  => Exec['system_update'],
+}
+
+file_line { 'add_http_header':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'location / {',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  require   => Package['nginx'],
+  subscribe => File_line['add_http_header'],
 }
