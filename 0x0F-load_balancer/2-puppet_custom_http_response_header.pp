a694c35788734d6a44f89677c10128882d628b18
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-07-17 11:48:11 -0700

    Puppet configure file for server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,25 +6,25 @@ exec { 'system_update':
 
 package { 'nginx':
    ensure => installed,
-   require => Exec['system_update']
+   require => Exec['system_update'],
 }
 
 file { '/var/www/html/index.html':
   content => 'Hello World!',
-  require => Package['nginx']
+  require => Package['nginx'],
 }
 
 exec { 'redirect_me':
-  command => 'sed -i '/listen 80 default_server/a \	rewrite ^/redirect_me https://intranet.alxswe.com/ permanent;' /etc/nginx/sites-available/default'
+  command => 'sed -i '/listen 80 default_server/a \	rewrite ^/redirect_me https://intranet.alxswe.com/ permanent;' /etc/nginx/sites-available/default',
   provider => 'shell',
-  require => Package['nginx']
+  require => Package['nginx'],
 }
 
 file_line { 'add_http_header':
   ensure => 'present',
   path => '/etc/nginx/sites-available/default',
   after => 'location / {',
-  line  => 'add_header X-Served-By $hostname;'
+  line  => 'add_header X-Served-By $hostname;',
   require => Package['nginx'],
 }
 
