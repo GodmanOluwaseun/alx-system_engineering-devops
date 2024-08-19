2479209033123289bd8c57a862677cf8af46eb9d
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-08-19 15:11:58 +0000

    query reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,13 @@ Queries the Reddit API and returns the number of subscribers
 """
 
 import requests
-import sys
 
 def number_of_subscribers(subreddit):
 
-    url = f"www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, allow_redirects=false)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    header = {"User-Agent": "Mozilla/5.0"}
+
+    response = requests.get(url, headers=header, allow_redirects=false)
 
     if response.status_code == 200:
         about = response.json()
