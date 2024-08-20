d4d1bcd7aac94ddd47bbb94b2be8e4cc55e7e98e
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-08-20 07:10:39 -0700

    Queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,12 +10,14 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns number of subscribers to subreddit"""
 
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     header = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/godmanoluwaseun)"
     }
 
     response = requests.get(url, headers=header, allow_redirects=False)
+
+    print("response code:", response.status_code)
 
     if response.status_code == 200:
         about = response.json()
