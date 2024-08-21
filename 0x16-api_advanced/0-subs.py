6e23f46e8a06593263f30a2f43c6bb8faf2fdf2f
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-08-21 04:18:42 -0700

    Updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns number of subscribers to subreddit"""
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     header = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/godmanoluwaseun)"
     }
